f21551323b0f6f9211fddc888607e40f299e0589
Scott Reinhart <34212688+scottdreinhart@users.noreply.github.com>
2023-01-17 03:28:15 -0500

    testing husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './styles/App.scss';
 import React from 'react';
 
 export function App() {
-  return <div className="bigRed">Hello, World!</div>;
+  return <div className="bigRed">Hello, World!!!</div>;
 }
 
 export default App;
